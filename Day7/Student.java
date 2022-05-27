6127e4c23e88cc9358d8e5c6cd2c954279bcf9b0
Riteshkumarjha98 <soumritjha@gmail.com>
2022-05-27 12:23:38 +0530

    update

---
+++ Day7/Student.java
@@ -0,0 +1,16 @@
+package com.Day7;
+
+public class Student {
+	int StudId;
+	String StudName;
+	double examFee;
+	
+	
+	
+	 public void displayData(){
+	
+		 public double PayFee(double Amount)
+	}
+
+}
+
